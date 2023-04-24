3264162467b546fc47b1829a972dbcfde8d6eef0
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-04-24 10:01:51 -0300

    insert data method

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,44 +10,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		// getData();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Connection conn = null;
-		
-		PreparedStatement st = null;
-
-		try {
-			conn = DB.getConnection();
-			/*st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);*/
-
-			st = conn.prepareStatement("INSERT INTO department (name) VALUES ('D1'),('D2')",Statement.RETURN_GENERATED_KEYS);
-			int rowsAffected = st.executeUpdate();
-
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			} else {
-				System.out.println("No rows affected!");
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		//} catch (ParseException e) {
-		//	e.printStackTrace();
-		} finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
-
+		//getData();
+		//insertData();
 	}
 
 	public static void getData() {
@@ -72,4 +36,42 @@ public class Program {
 		}
 	}
 
+	public static void insertData() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+
+			//st = conn.prepareStatement("INSERT INTO department (name) VALUES ('D1'),('D2')",Statement.RETURN_GENERATED_KEYS);
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			} else {
+				System.out.println("No rows affected!");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
 }
