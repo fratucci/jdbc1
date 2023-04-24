6e15386a351e8b4a0d0aa454100a28a726127a83
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-04-24 09:59:13 -0300

    insert depatment return key

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,18 +13,20 @@ public class Program {
 		// getData();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
+		
 		PreparedStatement st = null;
 
 		try {
 			conn = DB.getConnection();
-			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+			/*st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
+			st.setInt(5, 4);*/
 
+			st = conn.prepareStatement("INSERT INTO department (name) VALUES ('D1'),('D2')",Statement.RETURN_GENERATED_KEYS);
 			int rowsAffected = st.executeUpdate();
 
 			if (rowsAffected > 0) {
@@ -39,8 +41,8 @@ public class Program {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
+		//} catch (ParseException e) {
+		//	e.printStackTrace();
 		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
